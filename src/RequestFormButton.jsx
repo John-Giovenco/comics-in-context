9bcb5982809d2e879214013ee241fe55231b3138
John-Giovenco <JohnGiovenco93@gmail.com>
2023-05-20 14:39:29 -0700

    first commit

--- src/RequestFormButton.jsx
+++ src/RequestFormButton.jsx
@@ -1,14 +1,13 @@
 import React, { useContext } from "react";
+import StylesContext from "./StylesContext"
 
+export default function CollectorButton() {
+  const buttonStyling = React.useContext(StylesContext)
 
-export default function RequestFormButton() {
   return (
     <div>
-      <button>
-        Click Here!
-      </button>
-      <input type="submit" value="Submit" />
+      <input style={buttonStyling} type="submit" value="Submit" />
 
     </div>
   );
-}
+} 
